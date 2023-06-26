08564623ef62116171fbf27776624899f45a4bf7
Remy Malan <remy.malan@imiriba.com>
2023-06-25 21:55:32 -0700

    Buffer script updated.

---
+++ set_mtu_buffers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Usage: set_buffers.sh [interface]
+# These changes are lost at system restart.
+
+IF_DEFAULT=enp6s16
+
+# Set MTU for jumbo frames.
+
+if [ $# -eq 0 ] ; then
+  INTERFACE=$IF_DEFAULT
+else
+  INTERFACE="$1"
+fi
+
+echo "Set MTU on $INTERFACE"
+sudo ifconfig $INTERFACE mtu 9000 up
+
+# Set network buffers.
+
+sudo sysctl -w net.core.rmem_max=1024000
+sudo sysctl -w net.core.wmem_max=1024000
+sudo sysctl -w net.core.optmem_max=204800
+
