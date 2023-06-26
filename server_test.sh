7722355672ef7f939527f18bea8c0093f9357d30
Remy Malan <remy.malan@imiriba.com>
2023-06-25 21:09:28 -0700

    Addtional server test script changes.

--- server_test.sh
+++ server_test.sh
@@ -3,6 +3,17 @@
 # Server test. Run as daemon in background.
 # IP address is the interface for the server to respond on.
 
-iperf3 --server --bind 192.168.5.21 --daemon
+# Usage: server_test.sh [IP address]
 
+IP_DEFAULT="192.168.5.21"
+
+if [ $# -eq 0 ] ; then
+  IP_ADDR=$IP_DEFAULT
+else
+  IP_ADDR="$1"
+fi
+
+echo "Deploying server on $IP_ADDR as a background daemon."
+
+iperf3 --server --bind $IP_ADDR --daemon
 
