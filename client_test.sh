2f3caab45989b4cf4fc5fa4c904fc06f977a1a32
Remy Malan <remy.malan@imiriba.com>
2023-06-25 17:47:06 -0700

    Parameterizing test scripts.

--- client_test.sh
+++ client_test.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Usage: client_test.sh [IP address]
+
+IP_DEFAULT="192.168.5.21"
 LIST="1000M 2500M 5000M 10000M"
+LOG_FILE="/tmp/net-test-$(date +%Y-%m-%d-%H%M.%S).txt"
+
+if [ $# -eq 0 ] ; then
+  IP_ADDR=$IP_DEFAULT
+else
+  IP_ADDR="$1"
+fi
+
+echo "Logging results for $IP_ADDR in $LOG_FILE"
+echo "$0: Test results on $IP_ADDR" >> $LOG_FILE
 
 for RATE in $LIST; do
   echo "Testing $RATE..."
-  iperf3 --client 192.168.5.21 --time 30 --omit 15 --bidir --interval 0 --bitrate $RATE
+  iperf3 --client $IP_ADDR --time 30 --omit 15 --bidir --interval 0 --bitrate $RATE --logfile $LOG_FILE
 done
 
--- set_buffers.sh
+++ set_buffers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+INTERFACE=enp6s16
+
+# Set MTU for test.
+
+sudo ifconfig $INTERFACE mtu 9000 up
+
 # Set network buffers for test.
 # These changes are lost at system restart.
 
