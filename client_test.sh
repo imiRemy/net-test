456b378a1ed7d84518f1e954a88d6d2211747aed
Remy Malan <remy.malan@imiriba.com>
2023-07-01 16:38:46 -0700

    Adding SSH server and UFW to install process.

--- client_test.sh
+++ client_test.sh
@@ -2,20 +2,31 @@
 
 # Usage: client_test.sh [IP address]
 
-IP_DEFAULT="192.168.5.21"
-LIST="1000M 2500M 5000M 10000M"
-LOG_FILE="/tmp/net-test-$(date +%Y-%m-%d-%H%M.%S).txt"
+# Read in default values from ".env" file.
+
+source .env
+echo "Server environment variable: $SERVER_IP_DEFAULT"
+echo "Logging directory: $LOG_ROOT"
+echo "Test speeds: $TEST_SPEEDS"
+
+# Check for optionally supplied server IP address and override default if it exists.
 
 if [ $# -eq 0 ] ; then
-  IP_ADDR=$IP_DEFAULT
+  IP_ADDR=$SERVER_IP_DEFAULT
 else
   IP_ADDR="$1"
 fi
 
+# Set up logging.
+
+mkdir -p "$LOG_ROOT"
+LOG_FILE="$LOG_ROOT/net-test-$(date +%Y-%m-%d-%H%M.%S).txt"
 echo "Logging results for $IP_ADDR in $LOG_FILE"
 echo "$0: Test results on $IP_ADDR" >> $LOG_FILE
 
-for RATE in $LIST; do
+# Run tests.
+
+for RATE in $TEST_SPEEDS; do
   echo "Testing $RATE..."
   iperf3 --client $IP_ADDR --time 30 --omit 15 --bidir --interval 0 --bitrate $RATE --logfile $LOG_FILE
 done
--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+# net-test installation.
+
+# Update OS and get basic modules.
+
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install net-tools iperf3
+
+# If you want to run remote commands to control the server machine,
+# then also install SSH. To manually run on both client and server,
+# installing the SSH server can be commented out.
+# NOTE: this script does *not* start the SSH server.
+
+sudo apt-get -y install openssh-server
+
+# Make sure the ufw firewall is installed.
+# Enable the firewall, allow SSH and the iperf3 server port read in
+# from the .env environment variables file.
+
+sudo apt-get -y install ufw
+
+echo "Setting up ufw firewall..."
+source .env
+sudo ufw allow ssh
+sudo ufw allow $IPERF_SERVER_PORT
+sudo ufw enable
 
